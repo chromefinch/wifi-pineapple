a1d916b1d9198e18c2060def5ba2bd0c9a5d607d
chromefinch <38330372+chromefinch@users.noreply.github.com>
2024-08-30 13:06:36 -0400

    Update ClientKick.sh

--- ClientKick.sh
+++ ClientKick.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# https://hak5.github.io/mk7-docs/docs/rest/pineap/pineap/
 # trying to drop & block clients after 5 mins, which is hopefully enough time to get cred hashes, ideal for wpa enterprise and open portal
 # curl -X POST http://172.16.42.1:1471/api/login -d '{"username": "root", "password": "pazz"}'
 token="gen from line above"
@@ -9,8 +10,9 @@ clients=$(curl -X GET http://172.16.42.1:1471/api/pineap/clients -H "Authorizati
 for client in $clients; do 
         time=$(echo $client | sed 's/ /\n/g' | sed 's/""/ /g' | sed 's/"//g' | awk '{print $2}' | cut -c 11-)
         if (($time >= $timeout)); then
-            mac=$(echo $client | sed 's/ /\n/g' | sed 's/""/ /g' | sed 's/"//g' | awk '{print $1}' | cut -c 5-)
+            mac=$(echo $client | sed 's/ /\n/g' | sed 's/""/ /g' | sed 's/"//g' | awk '{print $1}' | cut -c 5-);
             curl -X PUT http://172.16.42.1:1471/api/pineap/filters/client/list -H "Accept:application/json" -H "Authorization: Bearer $token" -d '{"mac": "'"$mac"'"}'
             curl -X DELETE http://172.16.42.1:1471/api/pineap/clients/kick -H "Accept:application/json" -H "Authorization: Bearer $token" -d '{"mac": "'"$mac"'"}'
+            curl -X PUT http://172.16.42.1:1471/api/notifications -H "Accept:application/json" -H "Authorization: Bearer $token" -d '{"level": 0, "message": "'"$mac has been added to Deny List after $time seconds"'", "module_name": "Client Auto Deny"}'
         fi
     done
